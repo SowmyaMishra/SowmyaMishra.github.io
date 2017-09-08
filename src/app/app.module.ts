4b388178de21184854b0655726cd620efa55a6a1
Sowmya <myst.sowmya@gmail.com>
2017-09-08 21:58:20 +0530

    Initial Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent }  from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+
+  @NgModule({
+    declarations: [
+      AppComponent,
+      NavbarComponent,
+    
+    ],
+    imports: [
+      BrowserModule,
+    ],
+    providers: [],
+    bootstrap: [
+      AppComponent,
+      NavbarComponent,
+      ]
+  })
+  export class AppModule { }
+  
